c6474c3a3f5921cd8f230dbeceaa775e72eec157
independence-et <independence-et@yandex.ru>
2015-10-09 20:05:09 +0300

    H2T1upload

---
+++ homeworks/homework2/task1.py
@@ -0,0 +1,24 @@
+__author__ = 'independence'
+
+
+def plural(num, word):
+    words = ['утюг', 'утюга', 'утюгов', 'ложка', 'ложки', 'ложек', 'гармошка',
+             'гармошки', 'гармошек', 'чайник', 'чайника', 'чайников']
+    if word in words:
+        a = words.index(word)
+        if a in [1, 2, 4, 5, 7, 8, 10, 11]:
+            print('я так отказываюсь работать, вы и сами знаете падежи.')
+        elif 11 <= num % 100 <= 14:
+            a = a + 2
+        elif num % 10 == 1:
+            a = a
+        elif 2 <= num % 10 <= 4:
+            a = a + 1
+        else:
+            a = a + 2
+        print(num, words[a])
+    else:
+        print('Facepalm')
+user_word = str(input())
+user_num = int(input())
+plural(user_num, user_word)
