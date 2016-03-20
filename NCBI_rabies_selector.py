a135711f62db717cb6f91571d56a8df398121b29
independence-et <independence-et@yandex.ru>
2016-03-20 20:51:27 +0300

    inline comments to format

--- NCBI_rabies_selector.py
+++ NCBI_rabies_selector.py
@@ -26,37 +26,37 @@ Eurasia = ["China", "India", "Indonesia", "Pakistan", "Bangladesh", "Russia", "J
 input_data = open("identifiers.txt", "r").read().split("\n")
 for string in input_data:
     string = string.split(" ")
-    identifiers[string[0]] = string[1]  #now we have a list of Genbank numbers
+    identifiers[string[0]] = string[1]  # now we have a list of Genbank numbers
 todo = len(identifiers)
 for identifier in identifiers:
     process += 1
     link = "http://www.ncbi.nlm.nih.gov/nuccore/" + identifier
-    driver.get(link) #opening page of this strain
+    driver.get(link)  # opening page of this strain
     fstring = "feature_" + identifiers[identifier] + "_source_0"
-    time.sleep(2) #waiting for javascript execution
+    time.sleep(2)  # waiting for javascript execution
     features = driver.find_element_by_id(fstring).text
     location = re.findall("/country=\"(.*)\"", features)
     year = re.findall("/collection_date=\"(.*)\"", features)
-    if len(year) != 0 and len(location) != 0: #getting rid of samples without location or year
-        country = location[0].split(":") #obtaining only countryname from precision location
-        if country[0] in Eurasia: #getting rid of non-eurasian samples
+    if len(year) != 0 and len(location) != 0:  # getting rid of samples without location or year
+        country = location[0].split(":")  # obtaining only countryname from precision location
+        if country[0] in Eurasia:  # getting rid of non-eurasian samples
             isolate = re.findall("/isolate=\"(.*)\"", features)
             organism = re.findall("/organism=\"(.*)\"", features)
-            if len(isolate) != 0 and len(organism) != 0: #getting rid of sampes without or organism isolate name
+            if len(isolate) != 0 and len(organism) != 0:  # getting rid of sampes without or organism isolate name
                 if organism[0] == "Rabies virus":
                     Result_string = identifier + " Rabies virus strain " + isolate[0] + " " +\
                                     location[0] + " " + year[0] + "\n"
-                    Selected_identifiers.append(Result_string) #saving targeted data
+                    Selected_identifiers.append(Result_string)  # saving targeted data
                     score += 1
                     print(str(process) + " of " + str(todo) + " Scored: " + Result_string + " score = " + str(score))
                 else:
                     print(str(process) + " of " + str(todo) + " Not scored: unknown organism [" +
                           location[0] + " " + year[0] + "]")
-                    Manual.append(identifier) #for manual examination
+                    Manual.append(identifier)  # for manual examination
             else:
                 print(str(process) + " of " + str(todo) + " Not scored: unknown isolate or organism [" +
                       location[0] + " " + year[0] + "]")
-                Manual.append(identifier) #for manual examination
+                Manual.append(identifier)  # for manual examination
         else:
             print(str(process) + " of " + str(todo) + " Not scored: non Eurasian [" + location[0] + " " + year[0] + "]")
     else:
