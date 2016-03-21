67d3291b9b74eb14059da12d5eaee819c94435a1
independence-et <independence-et@yandex.ru>
2016-03-21 12:19:51 +0300

    Update NCBI_rabies_selector.py

--- NCBI_rabies_selector.py
+++ NCBI_rabies_selector.py
@@ -3,7 +3,6 @@ import time
 from selenium import webdriver
 __author__ = 'independence'
 
-
 score = 0
 process = 0
 normal = True
@@ -30,14 +29,13 @@ Large_countries = ["China", "India", "Russia", "Iran", "Saudi Arabia", "Kazakhst
 input_data = open("identifiers.txt", "r").read().split("\n")
 for string in input_data:
     string = string.split(" ")
-    identifiers[string[0]] = string[1]  # now we have a list of Genbank numbers
+    identifiers[string[0]] = string[1]  #now we have a list of Genbank numbers
 todo = len(identifiers)
 for identifier in identifiers:
     process += 1
     link = "http://www.ncbi.nlm.nih.gov/nuccore/" + identifier
     driver.get(link)  # opening page of this strain
     fstring = "feature_" + identifiers[identifier] + "_source_0"
-<<<<<<< HEAD
     time.sleep(1)  # waiting for javascript execution
     ct = 0
     while 8 < 9:
@@ -57,10 +55,6 @@ for identifier in identifiers:
         Wrong_identifiers.append(identifier)
         normal = True
         continue
-=======
-    time.sleep(2)  # waiting for javascript execution
-    features = driver.find_element_by_id(fstring).text
->>>>>>> a135711f62db717cb6f91571d56a8df398121b29
     location = re.findall("/country=\"(.*)\"", features)
     year = re.findall("/collection_date=\"(.*)\"", features)
     if len(year) != 0 and len(location) != 0:  # getting rid of samples without location or year
@@ -72,7 +66,6 @@ for identifier in identifiers:
                 if organism[0] == "Rabies virus":
                     Result_string = identifier + " Rabies virus isolate " + isolate[0] + " " + \
                                     location[0] + " " + year[0] + "\n"
-<<<<<<< HEAD
                     if country[0] in Large_countries and len(country) == 1:
                         Manual_unprecise.append(Result_string)  # for manual examination
                         print(str(process) + " of " + str(todo) + " Not scored: unprecise location [" +
@@ -81,29 +74,16 @@ for identifier in identifiers:
                         Selected_identifiers.append(Result_string)  # saving targeted data
                         score += 1
                         print(str(process) + " of " + str(todo) + " Scored: " + Result_string + " score = " + str(score))
-=======
-                    Selected_identifiers.append(Result_string)  # saving targeted data
-                    score += 1
-                    print(str(process) + " of " + str(todo) + " Scored: " + Result_string + " score = " + str(score))
->>>>>>> a135711f62db717cb6f91571d56a8df398121b29
                 else:
                     Result_string = identifier + " " + location[0] + " " + year[0] + "\n"
                     print(str(process) + " of " + str(todo) + " Not scored: unknown organism [" +
                           location[0] + " " + year[0] + "]")
-<<<<<<< HEAD
                     Manual_unknown.append(Result_string)  # for manual examination
-=======
-                    Manual.append(identifier)  # for manual examination
->>>>>>> a135711f62db717cb6f91571d56a8df398121b29
             else:
                 Result_string = identifier + " " + location[0] + " " + year[0] + "\n"
                 print(str(process) + " of " + str(todo) + " Not scored: unknown isolate or organism [" +
                       location[0] + " " + year[0] + "]")
-<<<<<<< HEAD
                 Manual_unknown.append(Result_string)  # for manual examination
-=======
-                Manual.append(identifier)  # for manual examination
->>>>>>> a135711f62db717cb6f91571d56a8df398121b29
         else:
             print(str(process) + " of " + str(todo) + " Not scored: non Eurasian [" + location[0] + " " + year[0] + "]")
     else:
